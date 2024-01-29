7e73a1cf85c4f0f69d8a2864614872305a15643b
Natalia Lopez <natigab.24@gmail.com>
2024-01-29 16:26:27 -0300

    agrego estilos de bootstrap y el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Container } from 'react-bootstrap';
+import Footer from './componentes/Footer';
 
 function App() {
 
+
   return (
     <>
-      
+    <Container className='my-4 mainPage'>
+      <h1 className='text-center display-3'>Lista de Tareas</h1>
+   
+    </Container>
+    <Footer></Footer>
     </>
   )
 }
