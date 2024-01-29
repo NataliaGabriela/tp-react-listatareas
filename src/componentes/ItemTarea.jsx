c841433000571f917369fb2790fc1287f6e866ec
Natalia Lopez <natigab.24@gmail.com>
2024-01-29 16:34:24 -0300

    agrego el componente ItemTarea

---
+++ src/componentes/ItemTarea.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+
+const ItemTarea = ({nombreTarea}) => {
+    return (
+        <ListGroup.Item className='d-flex justify-content-between'>{nombreTarea}
+        <Button variant="danger" onClick={()=> borrarTarea(nombreTarea)}>Borrar</Button></ListGroup.Item>
+    );
+};
+
+export default ItemTarea;
